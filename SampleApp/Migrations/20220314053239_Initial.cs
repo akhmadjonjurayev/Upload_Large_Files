18960461e7f2fceb51af122384abfafdd2168533
Ahmadjon <formyproject1999@gmail.com>
2022-03-15 16:02:55 +0500

    Initial

---
+++ SampleApp/Migrations/20220314053239_Initial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SampleApp.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "file");
+
+            migrationBuilder.CreateTable(
+                name: "File",
+                schema: "file",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Content = table.Column<byte[]>(nullable: true),
+                    UntrustedName = table.Column<string>(nullable: true),
+                    Note = table.Column<string>(nullable: true),
+                    Size = table.Column<long>(nullable: false),
+                    UploadDT = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_File", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "File",
+                schema: "file");
+        }
+    }
+}
